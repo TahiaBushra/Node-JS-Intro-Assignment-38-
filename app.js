0b22f81a531064957e67e95da4b9112d571ee665
TahiaBushra <bushratahia@gmail.com>
2024-02-18 20:52:40 +0600

    All done

--- app.js
+++ app.js
@@ -21,19 +21,46 @@
 // console.log(os.release());
 
 //
-const fs = require("fs");
-const readStream = fs.createReadStream("example.txt");
-let buffer = "";
+// const fs = require("fs");
+// const readStream = fs.createReadStream("example.txt");
+// let buffer = "";
 
-readStream.on("data", (chunk) => {
-  buffer += chunk;
+// readStream.on("data", (chunk) => {
+//   buffer += chunk;
+// });
+
+// readStream.on("end", () => {
+//   console.log("Data reading finished");
+//   console.log(buffer);
+// });
+
+// readStream.on("error", (err) => {
+//   console.log(err);
+// });
+
+const fs = require("fs");
+const { Transform } = require("stream");
+
+const upperCaseTransform = new Transform({
+  transform(chunk, encoding, callback) {
+    this.push(chunk.toString().toUpperCase());
+    callback();
+  },
 });
 
-readStream.on("end", () => {
-  console.log("Data reading finished");
-  console.log(buffer);
+const readStream = fs.createReadStream("input.txt");
+const writeStream = fs.createWriteStream("output.txt");
+
+readStream.pipe(upperCaseTransform).pipe(writeStream);
+
+writeStream.on("finish", () => {
+  console.log("Writing finished.");
 });
 
 readStream.on("error", (err) => {
+  console.log(err);
+});
+
+writeStream.on("error", (err) => {
   console.log(err);
 });
